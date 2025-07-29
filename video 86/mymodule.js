d5a7b2e437c35ebc526749176e86807551537b10
u7-c <aryansuneja121@gmail.com>
2025-07-29 17:54:07 +0530

    lrn node

--- video 86/mymodule.js
+++ video 86/mymodule.js
@@ -3,3 +3,9 @@ export const b=1;
 export const c=3;
 export const d=4;
 export const e=5;
+let obj;
+export default obj={//default export ko mai kisi bhi naam se import kru 
+    //udhr kuch frk nahi pdhega ki kis name se kiya hai
+    x: 5,
+    y: 7
+}
--- video 86/server.js
+++ video 86/server.js
@@ -1,2 +1,12 @@
-import a from "./mymodule.js"
-console.log(a);
+// import aryan from "./mymodule.js"
+// console.log(aryan);
+
+//important thing to note that we when we write type which is commonjs
+//which is also default 
+//it helps us to use require fxn 
+//wheras when we use type : module
+//we can use the keywords as shown above such as import and export as well
+
+const a = require ("./module2.js")
+console.log(a,__dirname,__filename)//shows the name of directory and file name too
+//jitne bhi modern syntax hai unme import hi use kiya jaata hai
