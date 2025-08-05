63a0752fe0bf3937f068430a83bcb9223b034b5e
u7-c <aryansuneja121@gmail.com>
2025-08-06 00:47:33 +0530

    completed 89

--- video 89/main.js
+++ video 89/main.js
@@ -1,25 +1,38 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const blog = require('./routes/blog')
+const shop = require('./routes/shop')
+ 
 
-app.use(express.static("public"));
 
-app.post('/', (req, res) => {
-    console.log("this is a post request");
-    res.send('hello world post');
-    //this is hits on my terminal when i open mypage.html on my browser and hits a post request
-}).put('/', (req, res) => {
-//basically what is happening is that the from the html its fetching / that means it i swaiting for a reply which we have
-//written in app.posty and when it rewplies it actually res.send logs something called hello world post
-    console.log("this is a put request");
-    res.send('hello world put');
-    //this is hits on my terminal when i open mypage.html on my browser and hits a post request
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+app.use('/blog', blog)
+app.use('/shop', shop)
+
+app.get('/', (req, res) => {
+    console.log("Hey its a get request")
+    res.send('Hello World21!')
+}).post('/', (req, res) => {
+    console.log("Hey its a post request")
+    res.send('Hello World post!')
 })
-app.get("/index",(req,res)=>{
-    console.log("HEY ITS INDEX");
-    res.sendFile('templates/index.html',{root:__dirname})//this returns the full directory path of the html file
+
+app.put('/', (req, res) => {
+    console.log("Hey its a put request")
+    res.send('Hello World put!')
 })
+
+app.get("/index", (req, res) => {
+    console.log("Hey its index")
+    res.sendFile('templates/index.html', { root: __dirname })
+})
+
+app.get("/api", (req, res) => {
+    res.json({ a: 1, b: 2, c: 3, d: 4, name: ["harry", "jerry"] })
+})
+
 app.listen(port, () => {
-    
-    console.log(`🚀 Server listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
